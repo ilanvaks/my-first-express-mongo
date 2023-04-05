1311fcba3c18676bcf3a6e246d681e2be7102707
IlanVaks <ilanvaks@gmail.com>
2023-04-05 14:44:33 -0400

    ADDED delete and update
    BUG fix update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import cors from "cors";
-import { getAllPlants, addPlant } from "./src/plants.js";
+import { getAllPlants, addPlant, deletePlant, updatePlant } from "./src/plants.js";
 
 const PORT = 3001
 
@@ -10,8 +10,15 @@ app.use(cors());
 app.use(express.json());
 
 // api points / routes will go here ... (req, res)
+app.get("/", (req, res) => {
+  res.send(`I am gRoot.`)
+});
+
+
 app.get("/plants", getAllPlants);
 app.post("/plants", addPlant);
+app.delete("/plants/:docId", deletePlant);
+app.patch("/plants/:docId", updatePlant)
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`)
